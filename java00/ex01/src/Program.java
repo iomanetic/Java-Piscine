879afafa8c34cd33c71ccf630a35a039ef871b1f
Karim Valiev <karimvaliev78@yandex.ru>
2022-07-09 19:54:44 +0300

    ex02 succes

---
+++ java00/ex01/src/Program.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner numIn = new Scanner(System.in);
+        int numDel = 2;
+        int num = numIn.nextInt();
+        int iterCount = 0;
+
+        if(num < 2) {
+            System.out.println("IllegalArgument\n");
+            return ;
+        }
+        if(num == 2) {
+            System.out.println("true 1" + '\n');
+            return ;
+        }
+        while(numDel * numDel <= num) {
+            iterCount++;
+            if(num % numDel == 0) {
+                System.out.println("false " + iterCount + '\n');
+                return ;
+            }
+            numDel++;
+        }
+        System.out.println("true " + iterCount + '\n');
+    }
+}
