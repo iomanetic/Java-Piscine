0c8d972abd923c81f31d95dbfdb4bddc33ab7131
Karim Valiev <karimvaliev78@yandex.ru>
2022-07-15 13:01:01 +0300

    ex02 succes

--- java03/ex02/src/main/java/org/example/Program.java
+++ java03/ex02/src/main/java/org/example/Program.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Random;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<Integer> arrayNumbs;
+        ThreadsCalculator[] threads;
+
         Random randomNum = new Random();
         int num;
         int lenOfArray;
         int numOfThreads;
+        int sum;
 
         if(args.length != 2 || !(args[0].startsWith("--arraySize=")
                                 && args[1].startsWith("--threadsCount"))) {
@@ -20,16 +23,53 @@ public class Program {
 
         lenOfArray = Integer.parseInt(args[0].substring(12));
         numOfThreads = Integer.parseInt(args[1].substring(15));
-        System.out.println(lenOfArray);
-        System.out.println(numOfThreads);
+        if((numOfThreads <= 0 || lenOfArray <= 0) || lenOfArray < numOfThreads) {
+            System.err.println("Invalid arguments");
+            System.exit(-1);
+        }
         arrayNumbs = new ArrayList<>(lenOfArray);
+        threads = new ThreadsCalculator[numOfThreads];
 
-        for(int count = 0; count < lenOfArray; count++) {
+        sum = 0;
+        for(int count = 0; count < lenOfArray - 1; count++) {
             num = randomNum.nextInt() % 1000;
             if(num < 0)
                 num *= -1;
             arrayNumbs.add(num);
+            sum += num;
         }
-        System.out.println(arrayNumbs);
+        System.out.println("Sum: " + sum);
+
+        int mod = lenOfArray % numOfThreads;
+        int to = 0;
+        int range;
+        int from;
+        for(int count = 0; count < numOfThreads; count++) {
+            from = to;
+            if(mod != 0) {
+                range = (lenOfArray - (numOfThreads - 1)) / (numOfThreads - 1);
+                if(range == 0) {
+                    range = 1;
+                }
+                to = (count + 1) * range;
+                if(count + 1 == numOfThreads) {
+                    to = (count + 1) * (lenOfArray / numOfThreads + 1);
+                }
+            } else {
+                to = (count + 1) * (lenOfArray / numOfThreads);
+            }
+            threads[count] = new ThreadsCalculator(from, to, count, arrayNumbs, numOfThreads);
+            threads[count].start();
+        }
+
+        for(int count = 0; count < numOfThreads; count++) {
+            threads[count].join();
+        }
+
+        sum = 0;
+        for(int count = 0; count < numOfThreads; count++) {
+            sum += threads[count].getSumOfNumber();
+        }
+        System.out.println("Sum by threads: " + sum);
     }
 }
--- java03/ex02/src/main/java/org/example/ThreadsCalculator.java
+++ java03/ex02/src/main/java/org/example/ThreadsCalculator.java
@@ -1,20 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadsCalculator extends Thread {
-
-    private int sumOfThread;
-
-    private int startIndex;
 
     private int endIndex;
 
-    public ThreadsCalculator(int startIndex, int endIndex) {
+    private int sumOfNumber;
+
+    private final int numOfThreads;
+
+    private final List<Integer> arrayList;
+
+    private final int threadNumber;
+
+    private final int startIndex;
+
+    public ThreadsCalculator(int startIndex, int endIndex, int threadNumber, List<Integer> arrayList, int numOfThreads) {
         this.startIndex = startIndex;
-        this.endIndex = endIndex;
+        if(endIndex > arrayList.size()) {
+            this.endIndex = arrayList.size();
+        } else {
+            this.endIndex = endIndex;
+        }
+        this.arrayList = arrayList;
+        this.sumOfNumber = 0;
+        this.threadNumber = threadNumber + 1;
+        this.numOfThreads = numOfThreads;
     }
 
-    public synchronized static void SumOfNumbers() {
+    @Override
+    public void run() {
+        for(int count = startIndex; count < endIndex; count++) {
+            sumOfNumber += arrayList.get(count);
+        }
+        if(this.numOfThreads == threadNumber) {
+            this.endIndex++;
+        }
+        printMessage();
+    }
 
+    public synchronized void printMessage() {
+        System.out.println("Thread " + this.threadNumber + ": "
+                            + "from " + this.startIndex + " to " + (this.endIndex - 1)
+                            + " sum is " + sumOfNumber);
+    }
+
+    public int getSumOfNumber() {
+        return (this.sumOfNumber);
     }
 
 }
