eff70a1b2299586b37c425c9795a2f7a17134c04
Karim Valiev <karimvaliev78@yandex.ru>
2022-07-19 19:20:29 +0300

    add java day 06 and complete ex00

---
+++ java06/ex00/Tests/src/test/java/edu/school21/numbers/NumberWorkerTest.java
@@ -0,0 +1,41 @@
+package edu.school21.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NumberWorkerTest {
+
+    NumberWorker numberWorker;
+
+    @BeforeEach
+    void getNewObject() {
+        numberWorker = new NumberWorker();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {127, 131 ,137, 139, 149, 151, 157, 163, 167, 173, 2})
+    void isPrimeForPrimes(int num) {
+        Assertions.assertTrue(numberWorker.isPrime(num));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18})
+    void isPrimeForNotPrimes(int num) {
+        Assertions.assertFalse(numberWorker.isPrime(num));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-4, -6, 0, 1, -10, -12, -14, -15, -16, -18})
+    void isPrimeForIncorrectNumbers(int num) {
+        Assertions.assertThrows(IllegalArgumentsException.class, () -> numberWorker.isPrime(num));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/data.csv"}, delimiter = ',')
+    void sumOfNumber(int x, int y) {
+        Assertions.assertEquals(numberWorker.digitSum(x), y);
+    }
+}
---
+++ java06/ex00/src/main/java/edu/school21/numbers/NumbWorker.java
@@ -0,0 +1,7 @@
+package edu.school21.numbers;
+
+public class NumbWorker {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
